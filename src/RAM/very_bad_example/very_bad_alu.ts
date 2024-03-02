16f4cb3f3bc674b90fa4883594897dfde8c41bfd
Adrián Mora Rodríguez <alu0101465883@ull.edu.es>
2024-03-02 20:10:50 +0000

    Actualizada la presentación

--- src/RAM/very_bad_example/very_bad_alu.ts
+++ src/RAM/very_bad_example/very_bad_alu.ts
@@ -1,14 +1,14 @@
 class Alu {
-  operate(a: number, b: number, operation: string): number {
+  operate(firstOperand: number, secondOperand: number, operation: string): number {
     switch (operation) {
       case 'add':
-        return a + b;
+        return firstOperand +secondOperand;
       case 'sub':
-        return a - b;
+        return firstOperand -secondOperand;
       case 'mul':
-        return a * b;
+        return firstOperand *secondOperand;
       case 'div':
-        return a / b;
+        return firstOperand /secondOperand;
       default:
         throw new Error('Operation not supported');
     }
